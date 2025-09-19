d9e66375ab7738bd0a709c97bf1cdfac8f00720d
AKUTHOTA SUSHANTH <smartysushanth67@gmail.com>
2025-09-19 12:02:03 +0000

    now telugu suspious words i will give for that words give a alert messag

--- src/lib/keywords.ts
+++ src/lib/keywords.ts
@@ -3,11 +3,16 @@ export const drugKeywords = [
   "drug", "weed", "cocaine", "ganja", "lsd", "molly", "hash", "meth",
   "pills", "packets", "powder", "cannabis", "score", "connect", "drop",
   "eight ball", "8-ball", "oz", "gram", "kilo", "mdma", "acid",
+  "marijuana", "dank", "snow", "blow", "coke", "flake", "coco", "smack", "brown sugar", "junk",
 
   // Emojis
   "🌿", "💊", "❄️", "💨", "🍄",
 
   // Tanglish (Tamil in English script)
   "machi", "joint", "joint raa", "joint erruka", "sarakku", "stuff",
-  "podi", "kudu", "vangi", "kidaikuma"
+  "podi", "kudu", "vangi", "kidaikuma",
+
+  // Telugu & other Indian slang
+  "mall", "saruku", "bungakku", "bhakka", "chutta", "sulfa",
+  "mama ganja unda raa", "mama mall unda", "bava joint kodudama"
 ];
