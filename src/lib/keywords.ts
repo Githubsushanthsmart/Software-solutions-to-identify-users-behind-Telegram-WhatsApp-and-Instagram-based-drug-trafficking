5127d3c792ffd2446ca1874226a5a2f3810315ac
AKUTHOTA SUSHANTH <smartysushanth67@gmail.com>
2025-09-19 11:37:41 +0000

    now in chat some people will use tamil language like machi joint raa and

--- src/lib/keywords.ts
+++ src/lib/keywords.ts
@@ -1,6 +1,13 @@
 export const drugKeywords = [
+  // English
   "drug", "weed", "cocaine", "ganja", "lsd", "molly", "hash", "meth",
-  "🌿", "💊", "❄️", "💨", "🍄",
   "pills", "packets", "powder", "cannabis", "score", "connect", "drop",
-  "eight ball", "8-ball", "oz", "gram", "kilo"
+  "eight ball", "8-ball", "oz", "gram", "kilo", "mdma", "acid",
+
+  // Emojis
+  "🌿", "💊", "❄️", "💨", "🍄",
+
+  // Tanglish (Tamil in English script)
+  "machi", "joint", "joint raa", "joint erruka", "sarakku", "stuff",
+  "podi", "kudu", "vangi", "kidaikuma"
 ];
